876f7b6fa5cada843b95c9a9f3ff648dba1b4ac4
juli <july_avy@hotmail.com>
2022-06-14 15:55:31 -0300

    fix: grafica auto escalable

--- prueba.py
+++ prueba.py
@@ -1,26 +1,14 @@
-import numpy as np
-from itertools import count
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from datetime import datetime
+from matplotlib import pyplot
+from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Button
+import random 
+from tkinter import *
+from tkinter import ttk
 
-fig, (ax1) = plt.subplots(1,1)
-x_data, y1_data  =[], []
-time1 = 0
-line, = ax1.plot([], [], lw=7, color ='r') 
+import matplotlib.animation as animacion
 
-def set_figure():
-    
-    fig.set_size_inches(6,4)
-    fig.suptitle('Titulo', fontsize=10 , color='red')
-
-    ax1.set_xlabel('Tiempo[s]',fontdict={'color':'white','weight':'bold','size' : 10})
-    ax1.set_ylabel('Pulsaciones [%]',fontdict={'color':'white','weight':'bold','size' : 14})
-     
-    ax1.tick_params(axis='both', which='major', labelsize=14)
-    ax1.tick_params(axis='both', which='minor', labelsize=12)
-  
-    ax1.set_yticks(np.arange(0, 35, 5))
-    ax1.set_xticks(np.arange(0, 60, 10))
 
 DEVICE_FILE = "/dev/ASMAN_driver"
 
@@ -39,23 +27,55 @@ def read_sensor():
 
     return value
 
+#objeto grafico
+class Index:
+        #inicializacion
+        def __init__(self):
+            self.figA = plt.figure()
+            self.figA.set_size_inches(10,8)
+            
+            
+            self.x_data = []
+            self.y_data = []
+            
+            self.line, = pyplot.plot_date(self.x_data, self.y_data, '-')
+            plt.style.use('ggplot')
+        
+            self.anim = FuncAnimation(self.figA, 
+                                            self.animada,   
+                                            interval=1000,
+                                            blit=True,frames=20, repeat=False)  
+      
+        #esta funcion se llama en en cada step
+        def animada(self,i):
+            valor1 = float(read_sensor())
+            print("Valor: ",valor1,"\n")
+            time1 = datetime.now()
+            self.x_data.append(time1)
+            self.y_data.append(valor1)
+            self.line.set_data(self.x_data, self.y_data)
+            self.figA.gca().relim()
+            self.figA.gca().autoscale_view()
+            return self.line,
+        
+        #grafica la señal indicada por channel
+        def graficar(self,channel): 
+            write(channel)
+            plt.show()
+            
 
-def graficar(self):
-    global time1
-    time1 += 3
-    valor1 = float(read_sensor())
-    x_data.append(time1)
-    y1_data.append(valor1)
-    print("Valor: ",valor1,"\n")        
-    line.set_data(x_data, y1_data) 
-    return line
-    
 
-valor = input("Ingrese un valor: ")
-write(valor)
-set_figure()
-animacion = animation.FuncAnimation(fig, graficar, interval=3000)
-plt.show()
+
+
+while(True):
+    channel=input("seleccione el canal \"a\" o \"b\": ")
+    while (channel!='a' and channel!='b' and channel!='exit' ):
+        channel=input("el channle "+channel+" no existe, elija \"a\", \"b\" o \"exit\" para salir: ")
+    if(channel=="exit"):
+            break    
+    grafica=Index()
+    grafica.graficar(channel)
+
 
     
 
